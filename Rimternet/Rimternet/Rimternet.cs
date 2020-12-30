58e2a0d496738eec093d503da927f3fdba06c3c2
Daze <44749931+Errleng@users.noreply.github.com>
2020-12-30 16:19:21 -0500

    Moved and updated HelpTab mod into Rimternet

--- Rimternet/Rimternet/Rimternet.cs
+++ Rimternet/Rimternet/Rimternet.cs
@@ -2,36 +2,22 @@
 using System.Reflection;
 using RimWorld;
 using Verse;
-using HarmonyLib;
 
 namespace Rimternet
 {
     public class Rimternet : Mod
     {
-        private RimternetSettings settings;
         public Rimternet(ModContentPack content) : base(content)
         {
-            this.settings = GetSettings<RimternetSettings>();
-        }
-        public override string SettingsCategory()
-        {
-            return "Rimternet".Translate();
+            LongEventHandler.QueueLongEvent(HelpBuilder.ResolveImpliedDefs, "BuildingHelpDatabase", false, null);
         }
     }
 
-    public class RimternetSettings : ModSettings
+    public static class ObjectExtension
     {
-    }
-
-    [StaticConstructorOnStartup]
-    public class Patcher
-    {
-        static Patcher()
+        public static string ToStringNullable(this object value)
         {
-            Harmony harmony = new Harmony("com.rimternet.rimworld.mod");
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            harmony.PatchAll(assembly);
-            Log.Message("Rimternet loaded");
+            return (value ?? "Null").ToString();
         }
     }
 }
---
+++ asdf/Rimternet/Rimternet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using RimWorld;
+using Verse;
+
+namespace Rimternet
+{
+    public class Rimternet : Mod
+    {
+        public Rimternet(ModContentPack content) : base(content)
+        {
+			LongEventHandler.QueueLongEvent (HelpBuilder.ResolveImpliedDefs, "BuildingHelpDatabase", false, null);
+        }
+    }
+}
