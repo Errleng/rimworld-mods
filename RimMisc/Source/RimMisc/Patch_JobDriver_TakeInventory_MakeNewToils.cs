cea6faae49dd113d8e077431127a3de82d684ebd
Daze <44749931+Errleng@users.noreply.github.com>
2021-07-20 16:17:56 -0400

    RimMisc - Disable Combat Extended specific patches

--- RimMisc/Source/RimMisc/Patch_JobDriver_TakeInventory_MakeNewToils.cs
+++ RimMisc/Source/RimMisc/Patch_JobDriver_TakeInventory_MakeNewToils.cs
@@ -1,27 +1,27 @@
-﻿using HarmonyLib;
-using RimWorld;
-using Verse;
-using Verse.AI;
+﻿//using HarmonyLib;
+//using RimWorld;
+//using Verse;
+//using Verse.AI;
 
-namespace RimMisc
-{
-    [HarmonyPatch(typeof(JobDriver_TakeInventory), "MakeNewToils")]
-    internal class Patch_JobDriver_TakeInventory_MakeNewToils
-    {
-        private static bool Prefix(JobDriver_TakeInventory __instance)
-        {
-            if (RimMisc.Settings.disableEnemyUninstall)
-            {
-                var pawn = __instance.pawn;
-                var thing = pawn.CurJob.GetTarget(TargetIndex.A).Thing;
-                if (!__instance.pawn.IsColonistPlayerControlled && thing.def.category == ThingCategory.Building && thing.Faction == Faction.OfPlayer)
-                {
-                    pawn.jobs.curDriver.ReadyForNextToil();
-                    return false;
-                }
-            }
+//namespace RimMisc
+//{
+//    [HarmonyPatch(typeof(JobDriver_TakeInventory), "MakeNewToils")]
+//    internal class Patch_JobDriver_TakeInventory_MakeNewToils
+//    {
+//        private static bool Prefix(JobDriver_TakeInventory __instance)
+//        {
+//            if (RimMisc.Settings.disableEnemyUninstall)
+//            {
+//                var pawn = __instance.pawn;
+//                var thing = pawn.CurJob.GetTarget(TargetIndex.A).Thing;
+//                if (!__instance.pawn.IsColonistPlayerControlled && thing.def.category == ThingCategory.Building && thing.Faction == Faction.OfPlayer)
+//                {
+//                    pawn.jobs.curDriver.ReadyForNextToil();
+//                    return false;
+//                }
+//            }
 
-            return true;
-        }
-    }
-}
+//            return true;
+//        }
+//    }
+//}
--- RimMisc/Source/RimMisc/Patch_Toils_Construct_UninstallIfMinifiable.cs
+++ RimMisc/Source/RimMisc/Patch_Toils_Construct_UninstallIfMinifiable.cs
@@ -1,42 +1,42 @@
-﻿using HarmonyLib;
-using RimWorld;
-using Verse;
-using Verse.AI;
+﻿//using HarmonyLib;
+//using RimWorld;
+//using Verse;
+//using Verse.AI;
 
-namespace RimMisc
-{
-    [HarmonyPatch(typeof(Toils_Construct), "UninstallIfMinifiable")]
-    class Patch_Toils_Construct_UninstallIfMinifiable
-    {
-        private static Toil Postfix(Toil toil)
-        {
-            if (RimMisc.Settings.disableEnemyUninstall)
-            {
-                var oldInitAction = toil.initAction;
-                toil.initAction = () =>
-                {
-                    var actor = toil.actor;
-                    var curDriver = actor.jobs.curDriver;
-                    //Log.Message($"initAction actor: {actor.Name} is a colonist: {actor.IsColonistPlayerControlled}");
-                    if (actor.IsColonistPlayerControlled)
-                        oldInitAction();
-                    else
-                        curDriver.ReadyForNextToil();
-                };
-                //var oldTickAction = toil.tickAction;
-                //toil.tickAction = () =>
-                //{
-                //    var actor = toil.actor;
-                //    var curDriver = actor.jobs.curDriver;
-                //    //Log.Message($"tickAction actor: {actor.Name} is a colonist: {actor.IsColonistPlayerControlled}");
-                //    if (actor.IsColonistPlayerControlled)
-                //        oldTickAction();
-                //    else
-                //        curDriver.ReadyForNextToil();
-                //};
-            }
+//namespace RimMisc
+//{
+//    [HarmonyPatch(typeof(Toils_Construct), "UninstallIfMinifiable")]
+//    class Patch_Toils_Construct_UninstallIfMinifiable
+//    {
+//        private static Toil Postfix(Toil toil)
+//        {
+//            if (RimMisc.Settings.disableEnemyUninstall)
+//            {
+//                var oldInitAction = toil.initAction;
+//                toil.initAction = () =>
+//                {
+//                    var actor = toil.actor;
+//                    var curDriver = actor.jobs.curDriver;
+//                    //Log.Message($"initAction actor: {actor.Name} is a colonist: {actor.IsColonistPlayerControlled}");
+//                    if (actor.IsColonistPlayerControlled)
+//                        oldInitAction();
+//                    else
+//                        curDriver.ReadyForNextToil();
+//                };
+//                //var oldTickAction = toil.tickAction;
+//                //toil.tickAction = () =>
+//                //{
+//                //    var actor = toil.actor;
+//                //    var curDriver = actor.jobs.curDriver;
+//                //    //Log.Message($"tickAction actor: {actor.Name} is a colonist: {actor.IsColonistPlayerControlled}");
+//                //    if (actor.IsColonistPlayerControlled)
+//                //        oldTickAction();
+//                //    else
+//                //        curDriver.ReadyForNextToil();
+//                //};
+//            }
 
-            return toil;
-        }
-    }
-}
+//            return toil;
+//        }
+//    }
+//}
