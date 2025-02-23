671487f55871728f31d5e81d6182167e8c304c00
Eric Liang <44749931+Errleng@users.noreply.github.com>
2025-02-23 17:35:49 -0500

    Farming mod

---
+++ AgriWorld/Source/AgriWorld.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace AgriWorld
+{
+    class AgriWorld : Mod
+    {
+        public AgriWorld(ModContentPack content) : base(content)
+        {
+            var harmony = new Harmony("com.agriworld.rimworld.mod");
+            harmony.PatchAll();
+        }
+
+        //[HarmonyPatch(typeof(PlantUtility), "CanSowOnGrower")]
+        //public class Patch_Sow
+        //{
+        //    static void Postfix(ThingDef plantDef, object obj, ref bool __result)
+        //    {
+        //        __result = true;
+        //    }
+        //}
+    }
+}
