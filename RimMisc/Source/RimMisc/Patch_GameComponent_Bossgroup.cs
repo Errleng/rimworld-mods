70d7683696c6a6059c11302b79cffb7d4e5fa3ea
Eric Liang <44749931+Errleng@users.noreply.github.com>
2025-08-29 22:06:23 -0700

    Add misc tweak to remove boss summon cooldown

---
+++ RimMisc/Source/RimMisc/Patch_GameComponent_Bossgroup.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimMisc
+{
+    internal class Patch_GameComponent_Bossgroup
+    {
+        [HarmonyPatch(typeof(GameComponent_Bossgroup), "Notify_BossgroupCalled")]
+        public static class Patch_Notify_BossgroupCalled
+        {
+            static void Postfix(GameComponent_Bossgroup __instance)
+            {
+                if (RimMisc.Settings.myMiscStuff)
+                {
+                    __instance.lastBossgroupCalled = 0;
+                }
+            }
+        }
+    }
+}
